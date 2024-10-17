e75e41944ac4affe411bc61119bb69498ebf3474
Châm <2251120403@ut.edu.vn>
2024-10-17 12:51:16 +0700

    Add files via upload

---
+++ front-end/JS/Singin.js
@@ -0,0 +1,17 @@
+﻿function cancelForm()
+{
+    var modal = document.getElementById('id01');
+    var form = document.getElementById('signupForm');
+    form.reset(); // Đặt lại form
+   
+}
+
+// Khi người dùng click bất kỳ đâu bên ngoài modal, đóng modal
+window.onclick = function (event)
+{
+    var modal = document.getElementById('id01');
+    if (event.target == modal)
+    {
+        modal.style.display = "none";
+    }
+}
